6be4e69d87f0f13d159b4ce81c1730f53c6c71f2
Martin Johnson <M.J.Johnson@massey.ac.nz>
2012-05-11 12:33:22 +1200

    add mddi_nt35510.o and decrease low memory killer addon

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -154,8 +154,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		if (other_free < (lowmem_minfree[i]+1024) &&
-		    other_file < (lowmem_minfree[i]+1024)) {
+		if (other_free < (lowmem_minfree[i]+512) &&
+		    other_file < (lowmem_minfree[i]+512)) {
 			min_adj = lowmem_adj[i];
 			break;
 		}
